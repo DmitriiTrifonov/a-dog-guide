4ec3b58f137819f8b909a9470c29d0f60ddc6db2
Dmitrii Trifonov <comanche.ak47@yandex.ru>
2019-12-07 22:53:31 +0300

    the first commit -- added two view controllers

---
+++ a-dog-guide/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  a-dog-guide
+//
+//  Created by Dmitrii Trifonov on 07.12.2019.
+//  Copyright © 2019 Dmitrii Trifonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var showAllBreeds: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
